1a8cca6dfa87eaa0a6449509d8428636a503b073
irene.bitlloch <irene.bitlloch@gmail.com>
2017-10-20 16:54:44 +0200

    Eliminiem elements de la llista obrint una finestra de text. Final primera fase.

--- app/src/main/java/com/example/irenebitlloch/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/irenebitlloch/shoppinglist/ShoppingListActivity.java
@@ -1,12 +1,18 @@
 package com.example.irenebitlloch.shoppinglist;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -44,15 +50,55 @@ public class ShoppingListActivity extends AppCompatActivity {
             }
         });
 
+        edit_item.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                addItem();
+                return true;
+            }
+        });
+
 
 
         list.setAdapter(adapter);
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?>  list, View item, int pos, long id) {
+                maybeRemoveItem(pos);
+                return true;
+            }
+        });
+
+    }
+
+    private void maybeRemoveItem(final int pos) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.confirm);
+        String fmt = getResources().getString(R.string.confirm_message);
+        builder.setMessage(String.format(fmt, itemList.get(pos)));
+
+
+        builder.setPositiveButton(R.string.remove, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                itemList.remove(pos);
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.create().show();
+
 
     }
 
     private void addItem() {
         String item_text = edit_item.getText().toString();
-        itemList.add(item_text);
-        adapter.notifyDataSetChanged();
+        if (!item_text.isEmpty()) {
+            itemList.add(item_text);
+            adapter.notifyDataSetChanged();
+            edit_item.setText("");
+        }
     }
 }
